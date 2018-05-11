565f11656109c5c38c2585c365a3c28d2afbe9b6
Jeroen Andress <jeroen.andress@onlinehome.de>
2018-05-11 15:04:54 +0200

    Add download of m4 files

--- Demo/demo.sh
+++ Demo/demo.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 cd ..
+
+if [ ! -f m4/ax_swig_python.m4 ]; then
+    echo "Download ax_swig_python.m4 ..."
+    wget -O m4/ax_swig_python.m4 "http://git.savannah.gnu.org/gitweb/?p=autoconf-archive.git;a=blob_plain;f=m4/ax_swig_python.m4"
+fi
+
+if [ ! -f m4/ax_python_devel.m4 ]; then
+    echo "Download ax_python_devel.m4 ..."
+    wget -O m4/ax_python_devel.m4 "http://git.savannah.gnu.org/gitweb/?p=autoconf-archive.git;a=blob_plain;f=m4/ax_python_devel.m4"
+fi
+
+if [ ! -f m4/ax_pkg_swig.m4 ]; then
+    echo "Download ax_pkg_swig.m4 ..."
+    wget -O m4/ax_pkg_swig.m4 "http://git.savannah.gnu.org/gitweb/?p=autoconf-archive.git;a=blob_plain;f=m4/ax_pkg_swig.m4"
+fi
+
 set -e
 INSTALLROOT=$PWD/install-root
 rm -rf build $INSTALLROOT
